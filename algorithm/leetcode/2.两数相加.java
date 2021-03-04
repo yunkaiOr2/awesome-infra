439c7529c5b50fa7c2df5ca671115ccc0eac0ebc
yunkai <qguduemc@163.com>
2021-03-04 08:25:26 +0800

    leetcode

--- algorithm/leetcode/2.两数相加.java
+++ algorithm/leetcode/2.两数相加.java
@@ -6,19 +6,44 @@
 
 // @lc code=start
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode() {} ListNode(int val) { this.val = val; } ListNode(int val,
+ * ListNode next) { this.val = val; this.next = next; } }
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode next = head;
+        boolean carry = false;
+        int sum = 0;
+
+        while (l1 != null || l2 != null) {
+            sum = 0;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            if (carry) {
+                ++sum;
+            }
+
+            next.next = new ListNode(sum % 10);
+            carry = sum >= 10;
+            next = next.next;
+        }
+
+        if (carry) {
+            next.next = new ListNode(1);
+        }
+
+        return head.next;
 
     }
 }
 // @lc code=end
-
